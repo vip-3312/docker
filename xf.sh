c16344fc21fe89320a51525a7a31ebe30dc091ea
vip-3312 <62170944+vip-3312@users.noreply.github.com>
2022-03-30 00:00:51 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "c99b7f21-f5a0-4b27-a901-82d47fddc76a",
                         "alterId": 0
                     }
                 ],
